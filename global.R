70f1782f6f8b799760bbd264fea046afa2549228
SimoneBrazzi <simonebrazzi@cloud.com>
2025-03-10 22:16:38 +0100

    add feature table

--- global.R
+++ global.R
@@ -107,13 +107,46 @@ query10 <- "SELECT desc_account_type, sign_transaction, SUM(amount_transaction) 
   GROUP BY desc_account_type, sign_transaction
   HAVING sign_transaction = '+'
   ORDER BY sign_transaction;"
+# feature table
+query11 <- "SELECT 
+    customers.id_customer,
+    DATE_DIFF('year', ANY_VALUE(customers.birthday), CURRENT_DATE) AS age,
+    COUNT(CASE WHEN transaction_type.sign = '-' THEN 1 ELSE NULL END) AS n_out_transactions,
+    COUNT(CASE WHEN transaction_type.sign = '+' THEN 1 ELSE NULL END) AS n_in_transactions,
+    SUM(CASE WHEN transaction_type.sign = '-' THEN transactions.amount ELSE 0 END) AS tot_out,
+    SUM(CASE WHEN transaction_type.sign = '+' THEN transactions.amount ELSE 0 END) AS tot_in,
+    COUNT(DISTINCT account.id_account) AS n_account,
 
-ids <- c("query1", "query2", "query3", "query4", "query5", "query6", "query7", "query8", "query9", "query10")
-queries <- c(query1, query2, query3, query4, query5, query6, query7, query8, query9, query10)
-texts<- c("Age of customer", "Number of outgoing transactions on all accounts", "Total amount transacted out on all accounts", "Total amount transacted incoming on all accounts", "Total number of accounts held", "Number of accounts held by type", "Number of outgoing transactions by account type", "Number of incoming transactions by account type", "Outgoing transacted amount by account type", "Amount transacted inbound by account type")
+    COUNT(CASE WHEN account_type.desc_account_type = 'Conto Privati' THEN 1 ELSE NULL END) AS n_private_account,
+    COUNT(CASE WHEN account_type.desc_account_type = 'Conto Base' THEN 1 ELSE NULL END) AS n_base_account,
+    COUNT(CASE WHEN account_type.desc_account_type = 'Conto Business' THEN 1 ELSE NULL END) AS n_business_account,
+    COUNT(CASE WHEN account_type.desc_account_type = 'Conto Famiglie' THEN 1 ELSE NULL END) AS n_family_account,
+
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Privati' AND transaction_type.sign = '-' THEN transactions.amount ELSE 0 END) AS tot_out_private_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Base' AND transaction_type.sign = '-' THEN transactions.amount ELSE 0 END) AS tot_out_base_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Business' AND transaction_type.sign = '-' THEN transactions.amount ELSE 0 END) AS tot_out_business_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Famiglie' AND transaction_type.sign = '-' THEN transactions.amount ELSE 0 END) AS tot_out_family_account,
+
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Privati' AND transaction_type.sign = '+' THEN transactions.amount ELSE 0 END) AS tot_in_private_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Base' AND transaction_type.sign = '+' THEN transactions.amount ELSE 0 END) AS tot_in_base_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Business' AND transaction_type.sign = '+' THEN transactions.amount ELSE 0 END) AS tot_in_business_account,
+    SUM(CASE WHEN account_type.desc_account_type = 'Conto Famiglie' AND transaction_type.sign = '+' THEN transactions.amount ELSE 0 END) AS tot_in_family_account
+  FROM customers
+  LEFT JOIN account ON customers.id_customer = account.id_customer
+  LEFT JOIN account_type ON account.id_account_type = account_type.id_account_type
+  LEFT JOIN transactions ON account.id_account = transactions.id_account
+  LEFT JOIN transaction_type ON transactions.id_transaction_type = transaction_type.id_transaction_type
+  GROUP BY customers.id_customer;
+"
+
+
+ids <- c("query1", "query2", "query3", "query4", "query5", "query6", "query7", "query8", "query9", "query10", "query11")
+queries <- c(query1, query2, query3, query4, query5, query6, query7, query8, query9, query10, query11)
+texts<- c("Age of customer", "Number of outgoing transactions on all accounts", "Total amount transacted out on all accounts", "Total amount transacted incoming on all accounts", "Total number of accounts held", "Number of accounts held by type", "Number of outgoing transactions by account type", "Number of incoming transactions by account type", "Outgoing transacted amount by account type", "Amount transacted inbound by account type", "Feature table for ML model")
 
 
 
 queries_lst <- setNames(queries, texts)
 
 onStop(\() dbDisconnect(con))
+
